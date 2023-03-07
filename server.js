386837eefbeacaa36f87f5a145a06ae5754fc570
Iman Abdurrahman <namasayaiman02@gmail.com>
2023-03-07 15:00:35 +0700

    yhuuu yuhhuuhu

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+  res.send("hello from Cloud Run!");
 });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log('Server listening on port ${PORT}...');
+  console.log("Server listening on port ${PORT}...");
 });
